d7c030781382a85b274b30cf3ef4b1f48f020e22
webeddies <edmani777@gmail.com>
2025-10-01 13:13:58 +0000

    finishing touches-4

--- src/components/ui/TestimonialCard.tsx
+++ src/components/ui/TestimonialCard.tsx
@@ -11,20 +11,16 @@ interface TestimonialCardProps {
 const TestimonialCard: React.FC<TestimonialCardProps> = ({ name, role, image, text }) => {
   return (
     <div className="bg-white p-6 rounded-xl shadow-lg hover:shadow-xl transition-all duration-300 min-w-[300px] mx-4">
-      <div className="flex items-center mb-4">
-        {[...Array(5)].map((_, i) => (
-          <Star key={i} size={16} className="text-yellow-400 fill-current" />
-        ))}
-      </div>
-      
+
+
       <div className="relative mb-4">
-        <Quote size={24} className="text-sky-blue absolute -top-2 -left-2" />
+        <Quote size={18} className="absolute -top-2 -left-2 text-[#B8960F]" />
         <p className="text-gray-700 italic pl-6">"{text}"</p>
       </div>
-      
+
       <div className="flex items-center">
-        <img 
-          src={image} 
+        <img
+          src={image}
           alt={name}
           className="w-12 h-12 rounded-full object-cover mr-4"
         />
